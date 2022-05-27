fb69c7550a91cb34dfacba5200af919e8e057155
Satyen-Sen <Satyendra.sant7@gmail.com>
2022-05-27 21:37:43 +0530

    added components

--- components/HeadPart.js
+++ components/HeadPart.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Head from 'next/head';
+import Head from 'next/head'; 
 
-function Home() {
+function HeadPart() {
 
     return (
         <Head>
             <title>Home App</title>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+            <link rel="shortcut icon" href="/static/favicon.ico"/>
         </Head>
     );
-}
+}; 
 
-export default Home; 
+export default HeadPart; 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { Box } from '@mui/system';
+import { Stack } from '@mui/material';
 
-import Home from '../src/Home';
+import HeadPart from '../components/HeadPart'; 
+import Feed from '../components/Feed';
+import Navbar from '../components/Navbar';
+import Rightbar from '../components/Rightbar';
+import Sidebar from '../components/Sidebar';
+
 
 function Index() {
   return (
-  <div>
-    <Home></Home>
-    test this
+    <Box>
+      <HeadPart/>
+      <Navbar/>
 
-  </div>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar></Sidebar>
+        <Feed></Feed>
+        <Rightbar></Rightbar>
+      </Stack>
+    </Box>
   );
-}
+}; 
 
 export default Index; 
 
