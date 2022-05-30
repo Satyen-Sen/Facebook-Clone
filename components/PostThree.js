5a0ba0c6b691fe8bc1b2fc8095eeba65c471d54f
Satyen-Sen <Satyendra.sant7@gmail.com>
2022-05-30 07:33:02 +0530

    updated ReadMe

--- components/PostThree.js
+++ components/PostThree.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Box } from '@mui/system';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-
+import Divider from '@mui/material/Divider';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
@@ -17,15 +17,15 @@ import ShareIcon from '@mui/icons-material/Share';
 
 function PostThree() {
     return (
-        <Card sx={{margin:2, height: 500}} >
+        <Card sx={{margin:2, height: 630}} >
             <CardHeader 
-                avatar={<Avatar alt="Priyanka-Nair" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGZhdCUyMGdpcmx8ZW58MHx8MHx8&w=1000&q=80"/>} 
-                title="Priyanka Nair" 
-                subheader="December 21, 2021"        
+                avatar={<Avatar alt="Vikram Singh" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQffWZ2g9m3DPjemXyhvwDlloQEbTR_PI9jbw&usqp=CAU"/>} 
+                title="Vikram Singh" 
+                subheader="February 28, 2022"        
             />
 
-            <Box sx={{ height: 800, overflowY: 'noscroll'}}>
-                <ImageList variant="masonry" cols={3} gap={8}>
+            <Box sx={{ height: 450, overflowY: 'noscroll', position:'relative', zIndex:1}}>
+                <ImageList variant="masonry" cols={3} gap={8} position="back">
                     {itemData.map((item) => (
                     <ImageListItem key={item.img}>
                         <img
@@ -38,23 +38,27 @@ function PostThree() {
                     ))}
                 </ImageList>
             </Box>
-
-            <CardContent>
-                <Typography>Biryani - Becoming a Desi Brand</Typography>
-                <Typography variant="body2" color="text.secondary"> 
-                    Biryani brings to the plate value, convenience and a satisfying experience at the same time. Companies have just scratched the surface of marketing the nuances of biryani, which is an emotion in India.
-                </Typography>
-            </CardContent>
-
+            <div>
+              <Divider sx={{ m:-2, background:'white', border:'none', height:'14px', position:'relative', zIndex:2 }} variant="top"/>  
+            </div>
+            <Typography sx={{mt:4, ml:3}}>My Ooty Trip Photos</Typography>
             <CardActions disableSpacing>
                 <IconButton><Checkbox icon={<FavoriteBorder/>} checkedIcon={<Favorite sx={{color:"red"}}/>}/></IconButton>          
-                <IconButton><Checkbox icon={<ShareIcon/>} checkedIcon={<ShareIcon/>}/></IconButton>              
-            </CardActions>
+                <IconButton><Checkbox icon={<ShareIcon/>} checkedIcon={<ShareIcon/>}/></IconButton>           
+            </CardActions> 
         </Card>
     );
 };
 
 const itemData = [
+      {
+        img: 'https://c.ndtvimg.com/gws/1341/assets/12.jpeg?1605605737',
+        title: 'Lake',
+      },
+      {
+        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_600,q_auto,w_auto/v1/filestore/gyko6nkmvh1a44v852wylp470ki3_1589460874_garden.jpg',
+        title: 'Resort',
+      },
       {
         img: 'https://vitaratours.com/wp-content/uploads/2021/04/i-love-ooty.jpg',
         title: 'Welcome',
@@ -64,20 +68,12 @@ const itemData = [
         title: 'HillTop',
       },
       {
-        img: 'https://c.ndtvimg.com/gws/1341/assets/1.jpeg?1605605737',
-        title: 'HillTop',
+        img: 'https://c.ndtvimg.com/gws/1341/assets/10.jpeg?1605605737',
+        title: 'train',
       },
       {
-        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_600,q_auto,w_auto/v1/filestore/gyko6nkmvh1a44v852wylp470ki3_1589460874_garden.jpg',
-        title: 'Resort',
-      },
-      {
-        img: 'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_600,q_auto,w_auto/v1/filestore/gyko6nkmvh1a44v852wylp470ki3_1589460874_garden.jpg',
-        title: 'Resort',
-      },
-      {
-        img: 'https://c.ndtvimg.com/gws/1341/assets/1.jpeg?1605605737',
-        title: 'HillTop',
+        img: 'https://www.trawell.in/admin/images/upload/208533138Ooty_Tea_Park.jpg',
+        title: 'park',
       },
     ];
 
